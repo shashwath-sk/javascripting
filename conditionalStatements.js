5de5794a5af2bfbeffc7dd374abf43639d30d7a9
shashwath sk <shashukarkera01@gmail.com>
2023-01-14 08:58:35 +0530

    feat(challenge9): add feature to differntiate by means of string length

---
+++ conditionalStatements.js
@@ -0,0 +1,8 @@
+let fruit = 'orange'
+let fruitLength = fruit.length
+if(length>5){
+    console.log("The fruit name has more than five characters. ")
+}
+else{
+    console.log("The fruit name has five characters or less.")
+}x
